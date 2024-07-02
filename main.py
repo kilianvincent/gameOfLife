48e04df9a968bbb0951a69d8c2791a17448e4e2f
kilianvincent <kilianvincen@gmail.com>
2024-07-02 16:50:35 +0200

    Grid functionality test

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from src.py.grid import *
+
+
 def main():
-    print('Hi!')
+    G = Grid(5)
+    G.set(2, 3, 1)
+    G.set(2, 2, 1)
+    G.set(2, 1, 1)
+
+    G.run()
 
 
 if __name__ == '__main__':
--- src/py/grid.py
+++ src/py/grid.py
@@ -1,8 +1,15 @@
+import time
+
+
 class Grid:
     def __init__(self, size):
         self.size = size
         self.grid = [[0 for x in range(self.size)] for y in range(self.size)]
         self.nextGrid = [[0 for x in range(self.size)] for y in range(self.size)]
+
+    def set(self, i, j, value):
+        if value == 1 or value == 0:
+            self.grid[i % self.size][j % self.size] = value
 
     def subgrid(self, i, j):
         return [[self.grid[(i-1) % self.size][(j+1) % self.size], self.grid[i % self.size][(j+1) % self.size], self.grid[(i+1) % self.size][(j+1) % self.size]],
@@ -22,3 +29,16 @@ class Grid:
                         self.nextGrid[i][j] = 1
         self.grid = self.nextGrid
         self.nextGrid = [[0 for x in range(self.size)] for y in range(self.size)]
+
+    def debug(self):
+        for row in self.grid:
+            for cell in row:
+                print(str(cell) + ' ', end='')
+            print()
+        print()
+
+    def run(self):
+        while True:
+            self.debug()
+            self.next()
+            time.sleep(1)
